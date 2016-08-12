974ac19571d9fde2fd3b8567c09076678d9928c0
Joel Scoble <joel.scoble@outlook.com>
2016-08-12 07:56:41 -0500

    initial bench commit

---
+++ benches.go
@@ -0,0 +1,382 @@
+package main
+
+import (
+	"testing"
+
+	dockeruuid "github.com/docker/distribution/uuid"
+	googleuuid "github.com/google/uuid"
+	hashicorpgouuid "github.com/hashicorp/go-uuid"
+	kasworldid "github.com/kasworld/idgen"
+	mitchellhuuid "github.com/mitchellh/packer/common/uuid"
+	"github.com/mohae/benchutil"
+	nu7hatchuuid "github.com/nu7hatch/gouuid"
+	rogpeppefastuuid "github.com/rogpeppe/fastuuid"
+	"github.com/rs/xid"
+	satorigouuid "github.com/satori/go.uuid"
+	sdminggosnow "github.com/sdming/gosnow"
+	"github.com/sony/sonyflake"
+	sumoryid "github.com/sumory/idgen"
+)
+
+func rsxID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		xid.New()
+	}
+}
+
+func BenchRSXID() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/rs/xid")
+	bench.Group = "pseudo-snowflake"
+	bench.SubGroup = "96 bits"
+	bench.Desc = "base32 hex encoded"
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(rsxID))
+	return bench
+}
+
+func kasworldIDGenUUID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		kasworldid.NewUUID()
+	}
+}
+
+func BenchKasworldIDGenUUID() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/kasworld/idgen")
+	bench.Group = "pseudo-uuid"
+	bench.SubGroup = "128-bit"
+	bench.Desc = "crypto/rand"
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(kasworldIDGenUUID))
+	return bench
+}
+
+func sumoryIDGen(b *testing.B) {
+	b.StopTimer()
+	err, w := sumoryid.NewIdWorker(0)
+	if err != nil {
+		return
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		w.NextId()
+	}
+}
+
+func BenchSumoryIDGen() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/sumory/idgen")
+	bench.Group = "snowflake"
+	bench.SubGroup = "64-bit"
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(sumoryIDGen))
+	return bench
+}
+
+var uuid = [16]byte{0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8}
+var name = []byte("name")
+
+func nu7hatchGoUUIDv3(b *testing.B) {
+	id := nu7hatchuuid.UUID(uuid)
+	for i := 0; i < b.N; i++ {
+		nu7hatchuuid.NewV3(&id, name)
+	}
+}
+
+func BenchNu7hatchGoUUIDv3() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/nu7hatch/gouuid")
+	bench.Group = "uuid v3"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(nu7hatchGoUUIDv3))
+	return bench
+}
+
+func nu7hatchGoUUIDv4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		nu7hatchuuid.NewV4()
+	}
+}
+
+func BenchNu7hatchGoUUIDv4() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/nu7hatch/gouuid")
+	bench.Group = "uuid v4"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(nu7hatchGoUUIDv4))
+	return bench
+}
+
+func nu7hatchGoUUIDv5(b *testing.B) {
+	id := nu7hatchuuid.UUID(uuid)
+	for i := 0; i < b.N; i++ {
+		nu7hatchuuid.NewV5(&id, name)
+	}
+}
+
+func BenchNu7hatchGoUUIDv5() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/nu7hatch/gouuid")
+	bench.Group = "uuid v5"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(nu7hatchGoUUIDv5))
+	return bench
+}
+
+func dockerUUID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		dockeruuid.Generate()
+	}
+}
+
+func BenchDockerUUID() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/docker/distribution/uuid")
+	bench.Group = "uuid v4"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(dockerUUID))
+	return bench
+}
+
+func MitchellhUUID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mitchellhuuid.TimeOrderedUUID()
+	}
+}
+
+func BenchMitchellhUUID() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/mitchellh/packer/common/uuid")
+	bench.Group = "psuedo-uuid"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(MitchellhUUID))
+	return bench
+}
+
+func SatoriGoUUIDv1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		satorigouuid.NewV1()
+	}
+}
+
+func BenchSatoriGoUUIDv1() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/satori/go.uuid")
+	bench.Group = "uuid v1"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(SatoriGoUUIDv1))
+	return bench
+}
+
+func SatoriGoUUIDv2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		satorigouuid.NewV2(0)
+	}
+}
+
+func BenchSatoriGoUUIDv2() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/satori/go.uuid")
+	bench.Group = "uuid v2"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(SatoriGoUUIDv2))
+	return bench
+}
+
+func SatoriGoUUIDv3(b *testing.B) {
+	s := string(name)
+	for i := 0; i < b.N; i++ {
+		satorigouuid.NewV3(uuid, s)
+	}
+}
+
+func BenchSatoriGoUUIDv3() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/satori/go.uuid")
+	bench.Group = "uuid v3"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(SatoriGoUUIDv3))
+	return bench
+}
+
+func SatoriGoUUIDv4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		satorigouuid.NewV4()
+	}
+}
+
+func BenchSatoriGoUUIDv4() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/satori/go.uuid")
+	bench.Group = "uuid v4"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(SatoriGoUUIDv4))
+	return bench
+}
+
+func SatoriGoUUIDv5(b *testing.B) {
+	s := string(name)
+	for i := 0; i < b.N; i++ {
+		satorigouuid.NewV5(uuid, s)
+	}
+}
+
+func BenchSatoriGoUUIDv5() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/satori/go.uuid")
+	bench.Group = "uuid v5"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(SatoriGoUUIDv5))
+	return bench
+}
+
+func RogPeppeFastUUID(b *testing.B) {
+	b.StopTimer()
+	g, err := rogpeppefastuuid.NewGenerator()
+	if err != nil {
+		return
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		g.Next()
+	}
+}
+
+func BenchRogPeppeFastUUID() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/rogpeppe/fastuuid")
+	bench.Group = "psuedo-uuid"
+	bench.SubGroup = "192-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(RogPeppeFastUUID))
+	return bench
+}
+
+func hashicorpGoUUID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		hashicorpgouuid.GenerateUUID()
+	}
+}
+
+func BenchHashicorpGoUUID() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/hashicorp/go-uuid")
+	bench.Group = "psuedo-uuid"
+	bench.SubGroup = "128-bit"
+	bench.Desc = "formatted as a uuid string"
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(hashicorpGoUUID))
+	return bench
+}
+
+func sonySonyFlake(b *testing.B) {
+	b.StopTimer()
+	var st sonyflake.Settings
+	st.MachineID = func() (uint16, error) {
+		return 42, nil
+	}
+	s := sonyflake.NewSonyflake(st)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.NextID()
+	}
+}
+
+func BenchSonySonyFlake() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/sony/sonyflake")
+	bench.Group = "psuedo-snowflake"
+	bench.SubGroup = "63-bit"
+	bench.Desc = "thread-safe"
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(sonySonyFlake))
+	return bench
+}
+
+func sdmingGosnow(b *testing.B) {
+	b.StopTimer()
+	s, err := sdminggosnow.NewSnowFlake(42)
+	if err != nil {
+		return
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Next()
+	}
+}
+
+func BenchSdmingGoSnow() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/0x6e6562/gosnow")
+	bench.Group = "snowflake"
+	bench.SubGroup = "64-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(sdmingGosnow))
+	return bench
+}
+
+func googleUUIDv1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		googleuuid.NewUUID()
+	}
+}
+
+func BenchGoogleUUIDv1() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/google/uuid")
+	bench.Group = "uuid v1"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(googleUUIDv1))
+	return bench
+}
+
+func googleUUIDv2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		googleuuid.NewDCEPerson()
+	}
+}
+
+func BenchGoogleUUIDv2() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/google/uuid")
+	bench.Group = "uuid v2"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(googleUUIDv2))
+	return bench
+}
+
+func googleUUIDv3(b *testing.B) {
+	bt := []byte(name)
+	for i := 0; i < b.N; i++ {
+		googleuuid.NewMD5(uuid, bt)
+	}
+}
+
+func BenchGoogleUUIDv3() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/google/uuid")
+	bench.Group = "uuid v3"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(googleUUIDv3))
+	return bench
+}
+
+func googleUUIDv4(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		googleuuid.NewRandom()
+	}
+}
+
+func BenchGoogleUUIDv4() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/google/uuid")
+	bench.Group = "uuid v4"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(googleUUIDv4))
+	return bench
+}
+
+func googleUUIDv5(b *testing.B) {
+	bt := []byte(name)
+	for i := 0; i < b.N; i++ {
+		googleuuid.NewSHA1(uuid, bt)
+	}
+}
+
+func BenchGoogleUUIDv5() benchutil.Bench {
+	bench := benchutil.NewBench("github.com/google/uuid")
+	bench.Group = "uuid v5"
+	bench.SubGroup = "128-bit"
+	bench.Desc = ""
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(googleUUIDv5))
+	return bench
+}
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/mohae/benchutil"
+)
+
+// flags
+var (
+	output         string
+	format         string
+	nameSections   bool
+	section        bool
+	sectionHeaders bool
+	systemInfo     bool
+)
+
+func init() {
+	flag.StringVar(&output, "output", "stdout", "output destination")
+	flag.StringVar(&output, "o", "stdout", "output destination (short)")
+	flag.StringVar(&format, "format", "txt", "format of output")
+	flag.StringVar(&format, "f", "txt", "format of output")
+	flag.BoolVar(&nameSections, "namesections", false, "use group as section name: some restrictions apply")
+	flag.BoolVar(&nameSections, "n", false, "use group as section name: some restrictions apply")
+	flag.BoolVar(&section, "sections", false, "don't separate groups of tests into sections")
+	flag.BoolVar(&section, "s", false, "don't separate groups of tests into sections")
+	flag.BoolVar(&sectionHeaders, "sectionheader", false, "if there are sections, add a section header row")
+	flag.BoolVar(&sectionHeaders, "h", false, "if there are sections, add a section header row")
+	flag.BoolVar(&systemInfo, "sysinfo", false, "add the system information to the output")
+	flag.BoolVar(&systemInfo, "i", false, "add the system information to the output")
+}
+
+func main() {
+	os.Exit(realMain())
+}
+
+func realMain() int {
+	flag.Parse()
+
+	// set up the ticker
+	done := make(chan struct{})
+	go benchutil.Dot(done)
+
+	// set the output
+	var w io.Writer
+	var err error
+	switch output {
+	case "stdout":
+		w = os.Stdout
+	default:
+		w, err = os.OpenFile(output, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer w.(*os.File).Close()
+	}
+	// get the benchmark for the desired format
+	// process the output
+	var bench benchutil.Benchmarker
+	switch format {
+	case "csv":
+		bench = benchutil.NewCSVBench(w)
+	case "md":
+		bench = benchutil.NewMDBench(w)
+	default:
+		bench = benchutil.NewStringBench(w)
+	}
+	bench.SectionPerGroup(section)
+	bench.SectionHeaders(sectionHeaders)
+	bench.IncludeSystemInfo(systemInfo)
+	bench.NameSections(nameSections)
+
+	// override column headers (if applicable)
+	bench.SetGroupColumnHeader("id type")
+	bench.SetSubGroupColumnHeader("bits")
+	bench.SetNameColumnHeader("package")
+	bench.SetDescColumnHeader("info")
+
+	// run the benchmarks and append the results
+	// snowflake
+	bench.Append(BenchSumoryIDGen())
+	bench.Append(BenchSdmingGoSnow())
+
+	// psuedo-snowflake
+	bench.Append(BenchRSXID())
+	bench.Append(BenchSonySonyFlake())
+
+	// uuid
+	bench.Append(BenchSatoriGoUUIDv1())
+	bench.Append(BenchGoogleUUIDv2())
+
+	bench.Append(BenchSatoriGoUUIDv2())
+	bench.Append(BenchGoogleUUIDv2())
+
+	bench.Append(BenchNu7hatchGoUUIDv3())
+	bench.Append(BenchSatoriGoUUIDv3())
+	bench.Append(BenchGoogleUUIDv3())
+
+	bench.Append(BenchDockerUUID())
+	bench.Append(BenchNu7hatchGoUUIDv4())
+	bench.Append(BenchSatoriGoUUIDv4())
+	bench.Append(BenchGoogleUUIDv4())
+
+	bench.Append(BenchNu7hatchGoUUIDv5())
+	bench.Append(BenchSatoriGoUUIDv5())
+	bench.Append(BenchGoogleUUIDv5())
+
+	// psuedo-uuid
+	bench.Append(BenchKasworldIDGenUUID())
+	bench.Append(BenchMitchellhUUID())
+	bench.Append(BenchRogPeppeFastUUID())
+	bench.Append(BenchHashicorpGoUUID())
+
+	// create the output
+	fmt.Println("")
+	fmt.Println("generating output...")
+	err = bench.Out()
+	if err != nil {
+		fmt.Printf("error generating output: %s\n", err)
+	}
+	return 0
+}
